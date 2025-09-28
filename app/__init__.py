7e6eb7e9689f214beb0a9c5874615867c73a2be1
KIrina06 <kirisha06@yandex.ru>
2025-09-28 19:21:34 +0300

    sth

--- app/__init__.py
+++ app/__init__.py
@@ -7,16 +7,15 @@ def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
         SQLALCHEMY_DATABASE_URI=os.environ.get(
-            'DATABASE_URL',
-            'postgresql://program:test@postgres:5432/persons'
+            "DATABASE_URL",
+            "postgresql://program:test@postgres:5432/persons"
         ),
         JSONIFY_PRETTYPRINT_REGULAR=False
     )
 
     init_db(app)
-    app.register_blueprint(bp, url_prefix='/api/v1')
+    app.register_blueprint(bp, url_prefix="/api/v1")
 
-    # 🔑 вот эта строчка решает проблему со слэшами
     app.url_map.strict_slashes = False
 
     return app
--- app/db.py
+++ app/db.py
@@ -1,7 +1,20 @@
+import time
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import OperationalError
+
 db = SQLAlchemy()
 
 def init_db(app):
     db.init_app(app)
     with app.app_context():
-        db.create_all()
+        retries = 5
+        while retries > 0:
+            try:
+                db.create_all()
+                break
+            except OperationalError:
+                retries -= 1
+                print("DB not ready, retrying in 2s...")
+                time.sleep(2)
+        else:
+            raise RuntimeError("Cannot connect to database after multiple retries")
